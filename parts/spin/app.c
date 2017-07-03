09b1db85f0cd21fe898af8485e790cb093de74ea
成田花奈 <ki1ch1an5@gmail.com>
2017-07-03 09:53:23 +0900

    その場でスピンだけするやつ

---
+++ parts/spin/app.c
@@ -0,0 +1,54 @@
+/*
+ *  モーター制御　サンプルプログラム02_sample_motor
+ */
+
+#include "ev3api.h"
+#include "app.h"
+
+#if defined(BUILD_MODULE)
+#include "module_cfg.h"
+#else
+#include "kernel_cfg.h"
+#endif
+
+/* モーターのポートを設定 */
+#define R_motor						EV3_PORT_B
+#define L_motor						EV3_PORT_C
+
+/* 待機時間の設定[msec] */
+const uint32_t RUN_TIME_MS = 3000;
+
+void run_task(intptr_t unused) {
+	int degrees;
+	int32_t speed_abs;
+	bool_t blocking;
+	static int32_t cnt = 0;
+	char msg[100];
+
+	// 回転角度をリセット
+	ev3_motor_reset_counts(L_motor);
+
+	// 前進
+	ev3_motor_rotate(R_motor, 720, 50, false);
+	ev3_motor_rotate(L_motor, 720, 50, false);
+	tslp_tsk(RUN_TIME_MS);
+
+	// スピン
+	ev3_motor_rotate(R_motor, 740, 50, false);
+	ev3_motor_rotate(L_motor, -740, 50, false);
+	tslp_tsk(RUN_TIME_MS);
+
+	// 停止
+	ev3_motor_stop(L_motor, true);
+	ev3_motor_stop(R_motor, true);
+	
+}
+
+void main_task(intptr_t unused) {
+	/* モーターの設定 */
+	ev3_motor_config( L_motor , LARGE_MOTOR );
+	ev3_motor_config( R_motor , LARGE_MOTOR );
+
+	/* タスクを開始する */
+	act_tsk(RUN_TASK);
+}
